1a634202aad0824a557af54c9b805658922a1d8b
lucidrains <lucidrains@gmail.com>
2025-06-26 07:38:11 -0700

    need a transformer and FSQ

---
+++ amplify_pytorch/amplify.py
@@ -0,0 +1,22 @@
+import torch
+from torch.nn import Module
+
+from x_transformers import (
+    Encoder,
+    Decoder,
+    TransformerWrapper
+)
+
+from vector_quantize_pytorch import FSQ
+
+# helpers
+
+def exists(v):
+    return v is not None
+
+# main class
+
+class Amplify(Module):
+    def __init__(self):
+        super().__init__()
+        raise NotImplementedError
