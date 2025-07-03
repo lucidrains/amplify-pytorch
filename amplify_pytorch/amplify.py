db80765c420a97425a5ad8aba7a3f79d31a6aedc
lucidrains <lucidrains@gmail.com>
2025-07-03 13:48:18 +0000

    sketch out overall idea, and start enjoying fruits of my labor

--- amplify_pytorch/amplify.py
+++ amplify_pytorch/amplify.py
@@ -10,6 +10,8 @@ from x_transformers import (
 
 from vector_quantize_pytorch import FSQ
 
+from einops import rearrange
+
 # helpers
 
 def exists(v):
@@ -23,7 +25,34 @@ class MotionTokenizer(Module):
         fsq_kwargs: dict = dict()
     ):
         super().__init__()
+        self.encoder = nn.Identity()
+
         self.fsq = FSQ(**fsq_kwargs)
+
+        self.decoder = nn.Identity()
+
+    def encode(
+        self,
+        motion_data
+    ):
+        encoded = self.encoder(motion_data)
+        return self.fsq(encoded)
+
+    @torch.no_grad()
+    def tokenize(self, motion_data):
+        _, token_ids = self.encode(motion_data)
+        return token_ids
+
+    def forward(
+        self,
+        motion_data
+    ):
+        quantized, indices = self.encode(motion_data)
+
+        recon = self.encoder(quantized)
+
+        recon_loss = F.mse_loss(data, recon)
+        return recon_loss
 
 class Amplify(Module):
     def __init__(
@@ -35,6 +64,33 @@ class Amplify(Module):
         super().__init__()
 
         self.tokenizer = tokenizer
+
+        self.embed = nn.Embedding(tokenizer.codebook_size, decoder.dim)
+
         self.decoder = decoder
 
         self.pool_to_actions = Attention(**action_cross_attn_pool_kwargs)
+
+    def forward(
+        self,
+        motion_data,
+        prepended_embeds
+    ):
+        token_ids = self.tokenizer.tokenize(motion_data)
+
+        tokens = self.embed(token_ids)
+
+        inp, target = tokens[:, :-1], tokens[:, 1:]
+
+        logits = self.decoder(
+            inp,
+            prepend_embeds = prepended_embeds
+        )
+
+        autoregressive_loss = F.cross_entropy(
+            rearrange(logits, 'b n l -> b l n'),
+            target,
+            ignore_index = -1
+        )
+
+        return  autoregressive_loss
